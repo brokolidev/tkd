b2af67bcfcae9244d56fbcb6496cdf6ecce4e488
brokoli <bocalist@gmail.com>
2025-03-10 13:31:37 -0600

    fixed api error

--- front/src/services/schdeuleServices.ts
+++ front/src/services/schdeuleServices.ts
@@ -1,6 +1,11 @@
 import axios from '@/lib/axios';
 
-export async function getSchedules(page: number = 1, pageSize: number = 6, openOnly: boolean = true) {
+export async function getSchedules(page: number | null = 1, pageSize: number = 6, openOnly: boolean = true) {
+    
+    if (page === null || page === undefined) {
+        page = 1
+    }
+    
     try {
         const response = await axios.get(
           '/schedules?pageNumber=' + page + '&pageSize=' + pageSize + '&openOnly=' + openOnly);
