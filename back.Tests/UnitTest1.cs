e3ae3d29bb9b7e4d8a611fa97da2b21f74684c2f
brokoli <bocalist@gmail.com>
2025-03-10 14:47:09 -0600

    installed nuget packages for testing

--- back.Tests/UnitTest1.cs
+++ back.Tests/UnitTest1.cs
@@ -1,10 +1,11 @@
-﻿namespace back.Tests;
-
-public class UnitTest1
+namespace back.Tests
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
 
+        }
     }
 }
