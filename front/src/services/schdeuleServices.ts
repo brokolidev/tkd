651bf5c4215c65666a09ee5278499e3f0ce8ff6a
narapark7561 <narapark7561@gmail.com>
2025-03-18 22:31:51 -0600

    add createSchedules service

--- front/src/services/schdeuleServices.ts
+++ front/src/services/schdeuleServices.ts
@@ -1,17 +1,33 @@
-import axios from '@/lib/axios';
+import axios from '@/lib/axios'
 
 export async function getSchedules(page: number | null = 1, pageSize: number = 6, openOnly: boolean = true) {
-    
-    if (page === null || page === undefined) {
-        page = 1
-    }
-    
-    try {
-        const response = await axios.get(
-          '/schedules?pageNumber=' + page + '&pageSize=' + pageSize + '&openOnly=' + openOnly);
-        return response.data;
-    } catch (e) {
-        console.error('Error fetching schedules:', e);
-        throw e;
-    }
+  if (page === null || page === undefined) {
+    page = 1
+  }
+
+  try {
+    const response = await axios.get(
+      '/schedules?pageNumber=' + page + '&pageSize=' + pageSize + '&openOnly=' + openOnly
+    )
+    return response.data
+  } catch (e) {
+    console.error('Error fetching schedules:', e)
+    throw e
+  }
+}
+
+export async function createSchedules(data: {
+  timeSlotId: number
+  day: string
+  studentIds: number[]
+  instructorIds: number[]
+  level: string
+  isOpen: boolean
+}) {
+  try {
+    const response = await axios.post('/schedules', data)
+    return response.data
+  } catch (e) {
+    console.error('Error creating schedule:', e)
+  }
 }
